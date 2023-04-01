0d7327499317e0bce75f3ad5f088923c48c8453e
Sana Rayess <rayess.sana@gmail.com>
2023-04-01 19:14:21 +0300

    added app.listen

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+app.use(express.json())
+
+const authRouter = require("./routes/auth.routes")
+app.use('/auth', authRouter)
+
+
+app.listen(process.env.PORT, (err) => {
+  if (err) console.error(err)
+  console.log("Server is running on port ", process.env.PORT);
+  require("./configs/db.config")
+});
