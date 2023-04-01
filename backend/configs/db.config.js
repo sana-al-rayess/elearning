89e08d62005b4fce7ceb5253113adea35c44f650
Sana Rayess <rayess.sana@gmail.com>
2023-04-02 00:56:16 +0300

    fixed typo and route

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -3,13 +3,25 @@ const app = express();
 require("dotenv").config();
 app.use(express.json())
 
-const authRouter = require("./routes/auth.routes")
-app.use('/auth', authRouter)
+// const authRouter = require("./routes/auth.routes")
+// app.use('/auth', authRouter)
 
 
 
-app.listen(process.env.PORT, (err) => {
-  if (err) console.error(err)
-  console.log("Server is running on port ", process.env.PORT);
-  require("./configs/db.config")
-});
+// app.listen(process.env.PORT, (err) => {
+//   if (err) console.error(err)
+//   console.log("Server is running on port ", process.env.PORT);
+//   require("./configs/db.config")
+// });
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection;
+db.on('error', (err) => console.error(err))
+
+db.once('open', () => console.log("MongoDB connected successfully!"));
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { login, register } = require("../controllers/auth.contollers")
+const { login, register } = require("../controllers/auth.controllers")
 
 router.get("/login", login);
 router.post("/register", register);
