871e9fc6d64564c9839e33d1ddef7c85a423c51f
Sana Rayess <rayess.sana@gmail.com>
2023-04-02 08:44:42 +0300

    fixed routes

--- backend/controllers/course.controllers.js
+++ backend/controllers/course.controllers.js
@@ -18,4 +18,12 @@ exports.addCourse = async (req, res) => {
     }
   };
 
-  
+  exports.getCourses = async (req, res) => {
+    try {
+      const courses = await Course.find();
+      res.status(200).json({ message: 'Courses retrieved successfully', courses });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server error', error });
+    }
+  };
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 const authRouter = require("./routes/auth.routes")
 app.use('/auth', authRouter)
 
+const userRouter = require("./routes/user.routes")
+app.use('/user', userRouter)
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.error(err)
