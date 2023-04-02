253629889207ea82a768cc2fb6e9c21c469f56d3
Sana Rayess <rayess.sana@gmail.com>
2023-04-02 09:58:38 +0300

    confugured electron

---
+++ frontend/public/electron.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const url = require('url');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Load the React app
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, '../build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+
+  // Open DevTools
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
